a8e0c84a82233843ec19985d384ac449c2994e4a
zhigulin_na <zhigulin_na@taximaxim.ru>
2015-02-16 17:13:33 +0500

    gulpfile, add requires, paths, server

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,53 @@
+'use strict';
 
+//NOTE Requires
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    cssmin = require('gulp-cssmin'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    connect = require('gulp-connect'),
+    opn = require('opn');
+
+//NOTE Paths
+var path = {
+    //NOTE path to finished files
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css',
+        img: 'build/img',
+        fonts: 'build/fonts'
+    },
+
+    //NOTE source path
+    src: {
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*',
+    },
+
+    //NOTE watch changes
+    watch: {
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*',
+    },
+
+    clean: '.build',
+};
+
+var server = {
+    host: 'localhost',
+    port: '9000'
+}
